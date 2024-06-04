c5bbcfd6688ee87c0d1573e7d14fdfe251eaefcd
Hariiko <lauraaltarribavillellas@gmail.com>
2024-06-04 12:04:37 +0200

    complete c00

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -1,13 +1,30 @@
 #include <unistd.h>
-
+#include <stdlib.h>
 
 void	ft_print_combn(int n)
 {
-    //01, 02, 03, 04, 05, 06, 07, 08, 09, 12, 13, 14, 15, 16, 17, 18, 19, 23, 24, 25, 26, 27, 28, 29, 34, 35, 36, 37, 38, 39, 45, 46, 47, 48, 49, 56, 57, 58, 59, 67, 68, 69, 78, 79, 89
+	int i = 48;
+	int j = 49;
+
+	while(i < 58)
+	{
+		int j = i +1;
+		while(j < 58)
+		{
+			write(1,&i,1);
+			write(1,&j,1);
+            if(i != 56 || j != 57)
+            {
+			    write(1,", ",1);
+            }
+            j++;
+		}
+		i++;
+	}
 }
 
-int	main(void)
+/*int main(void)
 {
-	ft_print_combn(2);
-	return (0);
-}
+    ft_print_combn(2);
+    return 0;
+}*/
